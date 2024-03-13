3fdb00c9bbd7cfc5d994373adadfbd2c7010ef00
mariogarzac <mariogarzac99@gmail.com>
2024-03-13 12:12:40 -0600

    Variables for the vm

---
+++ automation/terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "vmid" {
+  type    = string
+  default = ""
+}
+
+variable "name" {
+  type    = string
+  default = ""
+}
+
+variable "ipconfig" {
+  type    = string
+  default = ""
+}
+
+variable "password" {
+  type    = string
+  default = ""
+}
