9c70cb785fd29ad611e6a706c642c9dba19ef875
mariogarzac <mariogarzac99@gmail.com>
2024-03-13 13:28:23 -0600

    Three vms with docker and tailscale in 2 min

--- automation/terraform/main.tf
+++ automation/terraform/main.tf
@@ -1,7 +1,21 @@
 # Define module
-module "kube" {
+module "k8-1" {
   source   = "./kube"
-  vmid     = "106"
-  name     = "kube4"
-  ipconfig = "ip=192.168.68.106/24,gw=192.168.68.1"
+  vmid     = "201"
+  name     = "k8-1"
+  ipconfig = "gw=192.168.68.1,ip=192.168.68.103/22"
+}
+
+module "k8-2" {
+  source   = "./kube"
+  vmid     = "202"
+  name     = "k8-2"
+  ipconfig = "gw=192.168.68.1,ip=192.168.68.104/22"
+}
+
+module "k8-3" {
+  source   = "./kube"
+  vmid     = "203"
+  name     = "k8-3"
+  ipconfig = "gw=192.168.68.1,ip=192.168.68.105/22"
 }
