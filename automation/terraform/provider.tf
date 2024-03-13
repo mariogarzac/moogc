fed5568cb4c5c15a15c8325d03051edd91773595
mariogarzac <mariogarzac99@gmail.com>
2024-03-13 10:12:10 -0600

    Corrected indentation

--- automation/terraform/provider.tf
+++ automation/terraform/provider.tf
@@ -1,32 +1,32 @@
 terraform {
-    required_version = ">=1.5.7"
+  required_version = ">=1.5.7"
 
-    required_providers {
-        proxmox = {
-            source = "telmate/proxmox"
-            version = "3.0.1-rc1"
-       }
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "3.0.1-rc1"
     }
+  }
 }
 
 variable "proxmox_api_url" {
-    type = string
+  type = string
 }
 
 variable "proxmox_api_token_id" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 variable "proxmox_api_token_secret" {
-    type = string
-    sensitive = true
+  type      = string
+  sensitive = true
 }
 
 provider "proxmox" {
-    pm_api_url = var.proxmox_api_url
-    pm_api_token_id = var.proxmox_api_token_id
-    pm_api_token_secret = var.proxmox_api_token_secret
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
 
-    pm_tls_insecure = true
+  pm_tls_insecure = true
 }
